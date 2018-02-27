25ec66760a453c92975038f90c9f675721d51f28
tdoyle1 <trevorjdoyle1@gmail.com>
2018-02-27 09:17:19 -0600

    Tes our compareTo runner.

--- src/Feb26/FractionRunner.java
+++ src/Feb26/FractionRunner.java
@@ -2,5 +2,10 @@ package Feb26;
 
 public class FractionRunner
 {
-
+    public static void main (String[] args)
+    {
+        Fraction f1 = new Fraction( num 1, denom 4);
+        Fraction f2 = new Fraction( num 1, denom 3);
+        System.out.println
+    }
 }
